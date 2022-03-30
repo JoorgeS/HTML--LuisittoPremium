44aa796575e90ce3bb25b9c1dc2ef29222708ae8
JoorgeS <102179260+JoorgeS@users.noreply.github.com>
2022-03-29 23:50:52 -0300

    Actualización stylesheet css y JS
    se actualiza la hoja de css y se incorpora el archivo de Java Script

---
+++ Main.js
@@ -0,0 +1,15 @@
+document.querySelector('.menubtn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.carousel-inner', { delay: 500 });
+ScrollReveal().reveal('.main-content', { delay: 500 });
+ScrollReveal().reveal('.cards', { delay: 500 });
+ScrollReveal().reveal('.content-section', { delay: 500 });
+ScrollReveal().reveal('.tituloTyC', { delay: 500 });
+ScrollReveal().reveal('.parrafos', { delay: 500 });
+ScrollReveal().reveal('.Somos', { delay: 500 });
+ScrollReveal().reveal('.image-Logo', { delay: 500 });
+ScrollReveal().reveal('.headline', { delay: 500 });
+ScrollReveal().reveal('.headline', { delay: 500 });
